42c790eca278c96ecf90d2ebd55fe8d7741a9eb3
yukinimon <krischellecamama@gmail.com>
2023-03-04 13:23:29 +0800

    put a target in href and put the link in it

--- script/main.js
+++ script/main.js
@@ -1,25 +1,24 @@
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 200,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    loop: true,
-  });
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  loop: true,
+});
 
-
-  function toggleMenu() {
-      const menuToggle = document.querySelector('.toggle');
-      const navigation = document.querySelector('.navigation')
-      menuToggle.classList.toggle('active')
-      navigation.classList.toggle('active')
-  }
+function toggleMenu() {
+  const menuToggle = document.querySelector(".toggle");
+  const navigation = document.querySelector(".navigation");
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
